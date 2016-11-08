0cbae8143fa55cbc3310c72cd92fc94f0eba3daf
tloi <tloi@hotmail.com>
2016-11-08 03:14:36 -0500

    Updated

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -1,3 +1,5 @@
+  GNU nano 2.2.6                                              File: tournament.sql
+
 -- Table definitions for the tournament project.
 --
 -- Put your SQL 'create table' statements in this file; also 'create view'
@@ -6,4 +8,7 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+create table players (id SERIAL primary key, name TEXT);
+
+create table matches (id SERIAL primary key, winner INTEGER REFERENCES players (id), loser INTEGER REFERENCES players(id));
 
